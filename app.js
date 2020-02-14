66cd9013f02300eec134b6574f741cceb801649f
leul kashay <someone@nowhere.com>
2020-02-14 10:22:25 +0300

    start end route

--- app.js
+++ app.js
@@ -2,5 +2,35 @@ const express=require('express');
 
 const app=express();
 
+//cors 
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-origin',"*");
+    res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With,Content,Content-Type,Accepte,Authoriztion');
+    res.setHeader('Access-Control-Allow-Methods','PUT,GET,DELETE,POST,UPDATE');
+    next();
+});
+app.get('/api/stuff',(req,res,next)=>{
+    const stuff=[
+        {
+            _id: "w34werewr",
+            title: "apple",
+            description: "fresh fruit",
+            imageUrl: '',
+            price: 40,
+            userId: "235ewrwefsdsf43"
+        },
+        {
+            _id: "we433werwr",
+            title: "orange",
+            description: "fresh fruit",
+            imageUrl: '',
+            price:50,
+            userId: "tyuer634534wer43"
+        }
+    ]
+    res.status(200).json(stuff);
+})
+
+
 
 module.exports=app;
--- server.js
+++ server.js
@@ -2,7 +2,54 @@ const http=require('http');
 
 const app=require('./app');
 
+const normalizePort=val=>{
+    const port=parseInt(val,10);
+    if(isNaN(port)){
+        return val;
+    }
+    if(port >=0){
+        return port;
+    }
+    return false;
+}
+
+
+const port=normalizePort(process.env.PORT || 8000);
+
+app.set('port',process.env.PORT ||8000);
 const server=http.createServer(app);
 
+const errorHandler=error=>{
+    const address=server.address();
+    const bind=typeof address === 'string' ? 'pipe' + address : 'port' + port;
 
-server.listen(process.env.PORT || 8000);
+    if(error.syscall !== 'listen'){
+        throw error;
+    }
+
+    switch(error.code){
+        case 'EACCES':
+            console.log(bind + 'require elevated privelage');
+            process.exit(1);
+            break;
+        case 'EDDINUSE':
+            console.log(bind + 'is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+
+}
+
+server.on('error',errorHandler);
+
+server.on('listen',()=>{
+    const address=server.address();
+    const bind=typeof address === 'string' ? 'pipe' + address : 'port' + port;
+
+    console.log('listen on ' + bind);
+});
+
+
+server.listen(port);
